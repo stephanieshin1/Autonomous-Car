ac558c8ee74d1a7d016ae42c2cee790fc7631ccf
stephanieshin1 <49211123+stephanieshin1@users.noreply.github.com>
2020-09-20 10:36:53 -0700

    Added test2.py
    Test code to make sure bluetooth and pyserial are communicating correctly.

---
+++ test2.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import serial
+
+s = serial.Serial('/dev/cu.HC-05-DevB', timeout=None)
+if s.isOpen():
+    print("Bluetooth detected!")
+
+data = ""
+
+while data != 'STOP/':
+    data = s.read(s.read_until('/',)).decode("ascii")
+    print(data)
+
+print("Program ended.")
